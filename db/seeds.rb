8eb64187c6931037dbb75b8205f8138df6ede378
Shreya <shirkes088@gmail.com>
2025-12-08 15:12:18 +0530

    fixed readme and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-AdminUser.create!(email: 'admin2 	@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin2@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 puts "Deleting old data..."
 User.destroy_all
 Quiz.destroy_all
